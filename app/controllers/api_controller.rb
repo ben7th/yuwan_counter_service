ceef7f5344d11bbd0a04cf45e4f1e45abbd5c316
fushang318 <fushang318@gmail.com>
2015-02-04 14:47:56 +0800

    fix

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -15,7 +15,7 @@ class ApiController < ApplicationController
         :text => chat_line['room_id'],
         :username => chat_line['username'],
         :userlevel => chat_line['userlevel'],
-        :talk_time => talk_time
+        :talk_time => talk_time,
         :chat_type => chat_line['chat_type']
       )
     end
