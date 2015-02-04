129d3bcc7a40db1faaaaaad2c9456c41064d8132
arlyxiao <kingla_pei@163.com>
2015-02-04 14:14:59 +0800

    fix time format

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,13 +4,14 @@ class ApiController < ApplicationController
   def chat_lines
 
     params[:chat_lines].each do |chat_line|
+      talk_time = Time.at(chat_line['talk_time'] / 1000)
       ChatLine.create(
         :room_id => chat_line['room_id'],
         :manager => chat_line['manager'],
         :text => chat_line['room_id'],
         :username => chat_line['username'],
         :userlevel => chat_line['userlevel'],
-        :talk_time => chat_line['talk_time'],
+        :talk_time => talk_time
         :chat_type => chat_line['chat_type']
       )
     end
