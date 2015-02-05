4bde5855a5ee00d345002c45913e664c45b49275
fushang318 <fushang318@gmail.com>
2015-02-05 15:03:59 +0800

    fix

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -6,8 +6,8 @@ describe ApiController do
 
   it '接收 HTTP post 数据' do
     post :chat_lines, 
-          chat_lines: [
-          {
+          chat_lines: {
+          "0" => {
             room_id: 11, 
             manager: 'jane', 
             text: 'i speak', 
@@ -17,7 +17,7 @@ describe ApiController do
             chat_type: 'chat'
           },
 
-          {
+          "1" => {
             room_id: 11, 
             manager: 'jane', 
             text: 'i speak', 
@@ -26,7 +26,7 @@ describe ApiController do
             talk_time: Time.now,
             chat_type: 'chat'
           }
-        ]
+        }
 
         
     expect(ChatLine.all.length).to eq(2)
