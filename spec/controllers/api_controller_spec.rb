347d80d1d295fba98e31b398970607c962b7db12
arlyxiao <kingla_pei@163.com>
2015-02-06 15:31:29 +0800

    init rspec by_x data

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -36,55 +36,199 @@ describe ApiController do
 
 
   describe "统计数据" do
+    before {
+      @types = ['chat', 'welcome', 'forbid', 'yuwan']
+    }
 
-    describe "类型" do
+    describe "by_month" do
       before {
-        types = ['chat', 'welcome', 'forbid', 'yuwan']
+        @start_month = '2014-02'
+        @end_month = '2014-03'
 
-        Timecop.travel(Time.now - 1.day) do
-          
+        t = Time.local(2014, 2, 1, 0, 0, 0)
+        Timecop.travel(t) do
+
           3.times do |item|
             i = rand(4)
-            FactoryGirl.create(:chat_line, :room_id => '1', :chat_type => types[i])
+            FactoryGirl.create(:chat_line, :chat_type => @types[i])
           end
 
         end
 
-        Timecop.travel(Time.now - 5.day) do
+
+        t = Time.local(2014, 4, 1, 0, 0, 0)
+        Timecop.travel(t - 1.day) do
           
-          4.times do |item|
+          2.times do |item|
             i = rand(4)
-            FactoryGirl.create(:chat_line, :room_id => '2', :chat_type => types[i])
+            FactoryGirl.create(:chat_line, :chat_type => @types[i])
           end
 
         end
 
-        Timecop.travel(Time.now - 20.day) do
-          
-          3.times do |item|
-            i = rand(4)
-            FactoryGirl.create(:chat_line, :room_id => '3', :chat_type => types[i])
-          end
-
-        end
       }
 
-      it '准备数据' do
-        expect(ChatLine.all.length).to eq(10)
+
+      it '数量正确' do
+        expect(ChatLine.by_month(@start_month, @end_month).length).to eq(5)
       end
 
-      it 'by_room_id' do
-        expect(ChatLine.by_room_id(1).length).to eq(3)
-      end
-
-      it 'by_month' do
-        expect(ChatLine.by_month('2015-02-01', '2015-06-01').length).to eq(7)
-      end
-
-      it 'by_room_id && by_month' do
-        expect(ChatLine.by_room_id(1).by_month('2015-02-01', '2015-06-01').length).to eq(3)
-      end
     end
+
+
+    describe "by_week" do
+      before {
+        @start_week = '2014-12-W1'
+        @end_week = '2015-01-W4'
+
+        t = Time.local(2014, 12, 1, 0, 0, 0)
+        Timecop.travel(t) do
+
+          2.times do |item|
+            i = rand(4)
+            FactoryGirl.create(:chat_line, :chat_type => @types[i])
+          end
+
+        end
+
+
+        t = Time.local(2015, 2, 1, 0, 0, 0)
+        Timecop.travel(t - 1.day) do
+          
+          2.times do |item|
+            i = rand(4)
+            FactoryGirl.create(:chat_line, :chat_type => @types[i])
+          end
+
+        end
+
+      }
+
+
+      it '数量正确' do
+        expect(ChatLine.by_week(@start_week, @end_week).length).to eq(4)
+      end
+
+    end
+
+
+
+
+    describe "by_day" do
+      before {
+        @start_day = '2014-12-04'
+        @end_day = '2015-01-31'
+
+        t = Time.local(2014, 12, 4, 0, 0, 0)
+        Timecop.travel(t) do
+
+          1.times do |item|
+            i = rand(4)
+            FactoryGirl.create(:chat_line, :chat_type => @types[i])
+          end
+
+        end
+
+
+        t = Time.local(2015, 2, 1, 0, 0, 0)
+        Timecop.travel(t - 1.day) do
+          
+          2.times do |item|
+            i = rand(4)
+            FactoryGirl.create(:chat_line, :chat_type => @types[i])
+          end
+
+        end
+
+      }
+
+
+      it '数量正确' do
+        expect(ChatLine.by_day(@start_day, @end_day).length).to eq(3)
+      end
+
+    end
+
+
+
+
+
+    describe "by_hour" do
+      before {
+        @start_hour = '2014-12-04 21:'
+        @end_hour = '2015-01-16 08:'
+
+        t = Time.local(2014, 12, 4, 21, 0, 0)
+        Timecop.travel(t) do
+
+          1.times do |item|
+            i = rand(4)
+            FactoryGirl.create(:chat_line, :chat_type => @types[i])
+          end
+
+        end
+
+
+        t = Time.local(2015, 1, 16, 8, 0, 0)
+        Timecop.travel(t) do
+          
+          1.times do |item|
+            i = rand(4)
+            FactoryGirl.create(:chat_line, :chat_type => @types[i])
+          end
+
+        end
+
+      }
+
+
+      it '数量正确' do
+        expect(ChatLine.by_day(@start_hour, @end_hour).length).to eq(2)
+      end
+
+    end
+
+
+
+    describe "by_minute" do
+      before {
+        @start_minute = '2014-12-04 21:03'
+        @end_minute = '2015-01-16 08:27'
+
+        t = Time.local(2014, 12, 4, 21, 3, 0)
+        Timecop.travel(t) do
+
+          5.times do |item|
+            i = rand(4)
+            FactoryGirl.create(:chat_line, :chat_type => @types[i])
+          end
+
+        end
+
+
+        t = Time.local(2015, 1, 16, 8, 27, 0)
+        Timecop.travel(t) do
+          
+          2.times do |item|
+            i = rand(4)
+            FactoryGirl.create(:chat_line, :chat_type => @types[i])
+          end
+
+        end
+
+      }
+
+
+      it '数量正确' do
+        expect(ChatLine.by_day(@start_minute, @end_minute).length).to eq(7)
+      end
+
+    end
+
+
+
+
+
     
   end
 
