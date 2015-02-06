1ef7fb2d6ba7c18b2f1811a93b0ab7987c4e79a5
fushang318 <fushang318@gmail.com>
2015-02-06 15:43:18 +0800

    add str to time

---
+++ lib/str_time_util.rb
@@ -0,0 +1,162 @@
+class StrTimeUtil
+  class StringFormatError < StandardError;end
+
+  # '2014-12'    => time(2014-12-01 00:00:00 +0800)
+  def self.start_month_str_to_time(str)
+    match_data = str.match(/^([0-9]{4})-(0[1-9]|1[0-2])$/)
+    if match_data.blank?
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    year  = match_data[1].to_i
+    month = match_data[2].to_i
+    begin
+      time = Time.new(match_data[1], match_data[2])
+    rescue
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    if time.year != year
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    if time.month != month
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    time
+  end
+
+  # '2014-12'    => time(2015-01-01 00:00:00 +0800)
+  def self.end_month_str_to_time(str)
+    time = self.start_month_str_to_time(str)
+    time + 1.month
+  end
+
+  # '2015-02-W1' => time(2015-02-02 00:00:00 +0800)
+  def self.start_week_str_to_time(str)
+    match_data = str.match(/^([0-9]{4})-(0[1-9]|1[0-2])-W([1-5])$/)
+    if match_data.blank?
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+
+    year = match_data[1].to_i
+    month = match_data[2].to_i
+    week = match_data[3].to_i
+    begin
+      time = Time.new(year,month)
+    rescue
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    first_monday = time + ((time.wday-8).abs%7).day
+    start_week_time = first_monday+(week-1)*7.day
+    if start_week_time.month != month
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    start_week_time
+  end
+
+  # '2015-02-W1' => time(2015-02-09 00:00:00 +0800)
+  def self.end_week_str_to_time(str)
+    time = self.start_week_str_to_time(str)
+    time + 7.day
+  end
+
+  def self.start_day_str_to_time(str)
+    match_data = str.match(/^([0-9]{4})-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[0-1])$/)
+    if match_data.blank?
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    year = match_data[1].to_i
+    month = match_data[2].to_i
+    day = match_data[3].to_i
+    begin
+      time = Time.new(year,month,day)
+    rescue
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    if time.year != year
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    if time.month != month
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    if time.day != day
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    time
+  end
+
+  def self.end_day_str_to_time(str)
+    time = self.start_day_str_to_time(str)
+    time + 1.day
+  end
+
+  def self.start_hour_str_to_time(str)
+    match_data = str.match(/^([0-9]{4})-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[0-1]) (0[0-9]|1[0-9]|2[0-3]):$/)
+    if match_data.blank?
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    year  = match_data[1].to_i
+    month = match_data[2].to_i
+    day   = match_data[3].to_i
+    hour  = match_data[4].to_i
+    begin
+      time = Time.new(year,month,day,hour)
+    rescue
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    if time.year != year
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    if time.month != month
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    if time.day != day
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    if time.hour != hour
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    time
+  end
+
+  def self.end_hour_str_to_time(str)
+    time = self.start_hour_str_to_time(str)
+    time+1.hour
+  end
+
+  def self.start_minute_str_to_time(str)
+    match_data = str.match(/^([0-9]{4})-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[0-1]) (0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]|)$/)
+    if match_data.blank?
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    year  = match_data[1].to_i
+    month = match_data[2].to_i
+    day   = match_data[3].to_i
+    hour  = match_data[4].to_i
+    minute = match_data[5].to_i
+    begin
+      time = Time.new(year,month,day,hour,minute)
+    rescue
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    if time.year != year
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    if time.month != month
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    if time.day != day
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    if time.hour != hour
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    if time.min != minute
+      raise StringFormatError.new("时间字符串格式错误")
+    end
+    time
+  end
+
+  def self.end_minute_str_to_time(str)
+    time = self.start_minute_str_to_time(str)
+    time+1.minute
+  end
+end
---
+++ spec/str_time_util_spec.rb
@@ -0,0 +1,182 @@
+RSpec.describe StrTimeUtil do
+  describe 'str_to_time' do
+    describe 'month_str' do
+      it 'start_month_str_to_time' do
+        # '2014-12'    => time(2014-12-01 00:00:00 +0800)
+        str = '2014-12'
+        time = StrTimeUtil.start_month_str_to_time(str)
+        expect(time).to eq(Time.new(2014, 12))
+
+        str = '2014-13'
+        expect { 
+          StrTimeUtil.start_month_str_to_time(str)
+        }.to raise_error StrTimeUtil::StringFormatError
+      end
+
+      it "end_month_str_to_time" do
+        # '2014-12'    => time(2015-01-01 00:00:00 +0800)
+        str = '2014-12'
+        time = StrTimeUtil.end_month_str_to_time(str)
+        expect(time).to eq(Time.new(2015, 1))
+
+        str = '2014-13'
+        expect { 
+          StrTimeUtil.end_month_str_to_time(str)
+        }.to raise_error StrTimeUtil::StringFormatError
+      end
+    end
+
+    describe 'week_str' do
+      it 'start_week_str_to_time' do
+        # '2015-02-W1' => time(2015-02-02 00:00:00 +0800)
+        str = '2015-02-W1'
+        time = StrTimeUtil.start_week_str_to_time(str)
+        expect(time).to eq(Time.new(2015, 2,2))
+
+        str = '2015-13-W1'
+        expect { 
+          StrTimeUtil.start_week_str_to_time(str)
+        }.to raise_error StrTimeUtil::StringFormatError
+
+        str = '2015-02-A1'
+        expect { 
+          StrTimeUtil.start_week_str_to_time(str)
+        }.to raise_error StrTimeUtil::StringFormatError
+
+        str = '2015-02-W5'
+        expect { 
+          StrTimeUtil.start_week_str_to_time(str)
+        }.to raise_error StrTimeUtil::StringFormatError
+      end
+
+      it 'end_week_str_to_time' do
+        # '2015-02-W1' => time(2015-02-09 00:00:00 +0800)
+        str = '2015-02-W1'
+        time = StrTimeUtil.end_week_str_to_time(str)
+        expect(time).to eq(Time.new(2015, 2,9))
+
+        str = '2015-13-W1'
+        expect { 
+          StrTimeUtil.start_week_str_to_time(str)
+        }.to raise_error StrTimeUtil::StringFormatError
+
+        str = '2015-02-A1'
+        expect { 
+          StrTimeUtil.start_week_str_to_time(str)
+        }.to raise_error StrTimeUtil::StringFormatError
+
+        str = '2015-02-W5'
+        expect { 
+          StrTimeUtil.start_week_str_to_time(str)
+        }.to raise_error StrTimeUtil::StringFormatError
+      end
+    end
+
+    describe 'day_str' do
+      it 'start_day_str_to_time' do
+        # '2015-02-05' => time(2015-02-05 00:00:00 +0800)
+        str = '2015-02-05'
+        time = StrTimeUtil.start_day_str_to_time(str)
+        expect(time).to eq(Time.new(2015,2,5))
+
+        str = '2015-02-30'
+        expect { 
+          StrTimeUtil.start_day_str_to_time(str)
+        }.to raise_error StrTimeUtil::StringFormatError
+
+        str = '2015-03-32'
+        expect { 
+          StrTimeUtil.start_day_str_to_time(str)
+        }.to raise_error StrTimeUtil::StringFormatError
+      end
+
+      it 'end_day_str_to_time' do
+        # '2015-02-05' => time(2015-02-06 00:00:00 +0800)
+        str = '2015-02-05'
+        time = StrTimeUtil.end_day_str_to_time(str)
+        expect(time).to eq(Time.new(2015,2,6))
+
+        str = '2015-02-30'
+        expect { 
+          StrTimeUtil.end_day_str_to_time(str)
+        }.to raise_error StrTimeUtil::StringFormatError
+
+        str = '2015-03-32'
+        expect { 
+          StrTimeUtil.end_day_str_to_time(str)
+        }.to raise_error StrTimeUtil::StringFormatError
+      end
+    end
+    
+    describe 'hour_str' do
+      it 'start_hour_str_to_time' do
+        # '2015-02-05 15:' => time(2015-02-05 15:00:00 +0800)
+        str = '2015-02-05 15:'
+        time = StrTimeUtil.start_hour_str_to_time(str)
+        expect(time).to eq(Time.new(2015,2,5,15))
+
+        str = '2015-02-05 19'
+        expect { 
+          StrTimeUtil.start_hour_str_to_time(str)
+        }.to raise_error StrTimeUtil::StringFormatError
+
+        str = '2015-02-05 26:'
+        expect { 
+          StrTimeUtil.start_hour_str_to_time(str)
+        }.to raise_error StrTimeUtil::StringFormatError
+      end
+
+      it 'end_hour_str_to_time' do
+        # '2015-02-05 15:' => time(2015-02-05 16:00:00 +0800)
+        str = '2015-02-05 15:'
+        time = StrTimeUtil.end_hour_str_to_time(str)
+        expect(time).to eq(Time.new(2015,2,5,16))
+
+
+        str = '2015-02-05 19'
+        expect { 
+          StrTimeUtil.end_hour_str_to_time(str)
+        }.to raise_error StrTimeUtil::StringFormatError
+
+        str = '2015-02-05 26:'
+        expect { 
+          StrTimeUtil.end_hour_str_to_time(str)
+        }.to raise_error StrTimeUtil::StringFormatError
+      end
+    end
+
+    describe 'minute_str' do
+      it 'start_minute_str_to_time' do
+        # '2015-02-05 15:01' => time(2015-02-05 15:01:00 +0800)
+        str = '2015-02-05 15:01'
+        time = StrTimeUtil.start_minute_str_to_time(str)
+        expect(time).to eq(Time.new(2015,2,5,15,1))
+
+        str = '2015-02-05 15:61'
+        expect {
+          StrTimeUtil.start_minute_str_to_time(str)
+        }.to raise_error StrTimeUtil::StringFormatError
+        str = '2015-02-05 15:70'
+        expect {
+          StrTimeUtil.start_minute_str_to_time(str)
+        }.to raise_error StrTimeUtil::StringFormatError
+      end
+
+      it 'end_minute_str_to_time' do
+        # '2015-02-05 15:01' => time(2015-02-05 15:02:00 +0800)
+        str = '2015-02-05 15:01'
+        time = StrTimeUtil.end_minute_str_to_time(str)
+        expect(time).to eq(Time.new(2015,2,5,15,2))
+
+        str = '2015-02-05 15:61'
+        expect {
+          StrTimeUtil.end_minute_str_to_time(str)
+        }.to raise_error StrTimeUtil::StringFormatError
+        str = '2015-02-05 15:70'
+        expect {
+          StrTimeUtil.end_minute_str_to_time(str)
+        }.to raise_error StrTimeUtil::StringFormatError
+      end
+    end
+  end
+end
